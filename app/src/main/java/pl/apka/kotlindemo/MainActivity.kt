5159039a2ce3c25bf8211232dcfc7e32804bac31
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-19 15:52:50 +0100

    counter

--- app/src/main/java/pl/apka/kotlindemo/MainActivity.kt
+++ app/src/main/java/pl/apka/kotlindemo/MainActivity.kt
@@ -2,15 +2,30 @@ package pl.apka.kotlindemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
+
+    fun reset(view : View){
+        count = 0
+        textView.setText(count.toString())
+    }
+    fun plusOne(view : View){
+        count++
+        textView.setText(count.toString())
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var textView = findViewById<TextView>(R.id.textView)
-        textView.setText("Hello")
+        textView.setText("ZERO")
     }
 }
